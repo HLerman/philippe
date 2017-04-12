eddc22a45b9c0268bbd85556a038dd0613175f5e
Lerman <contact@lerman.fr>
2017-04-12 21:54:38 +0200

    PH-03 : add new func : manage all of your quotes with json files

--- README.md
+++ README.md
@@ -1,12 +1,33 @@
-#Philippe
+# Philippe
+
+Change your shell prefix with a lot of movies quotes.
+
+**Requirments**
+
+ - Perl >= 5.14
 
 **How to use Philippe ?**
 
-    git clone https://github.com/HLerman/philippe.git
-    vim ~/.bashrc
+     git clone https://github.com/HLerman/philippe.git
+     vim ~/.bashrc
 
 Add this line at the end of the file
 
      PS1='$(perl /path/of/philippe/philippe.pl) '
 
-Save and reload bashrc file.
+Save and reload bashrc file.  
+
+**How can add my own quotes ?**
+Create a new json file in the quote folder and use this template :
+
+    {
+        "name": "title like Terminator or Barbar",
+        "quote": [
+            "First quote",
+            "Seconde quote",
+            "Thrid quote",
+            "..."
+    ]
+}
+
+Your file will be automatically used.
--- philippe.pl
+++ philippe.pl
@@ -1,12 +1,29 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use Encode qw(encode_utf8);
+use JSON::PP;
 
-my @quote = (
-    "Philippe, je sais où tu t'caches !",
-    "Viens ici que j'te bute enculé",
-    "Ta gueule",
-    "Salaud !",
-);
+my $quoteFolder = './quote/';
 
-print $quote[rand(scalar(@quote))]."\n";
+opendir(my $dh, $quoteFolder) or die "Cannot access to the quote folder $quoteFolder";
+my @files = grep { /\.json$/ } readdir($dh);
+my $file = $files[rand(scalar(@files))];
+
+if(not -z "$quoteFolder$file")
+{
+    open(my $fh, "$quoteFolder$file") or die "Cannot open the file $file";
+        my $json = '';
+        while(defined(my $l = <$fh>))
+        {
+            $json .= $l;
+        }
+        $json = decode_json encode_utf8($json);
+        print $json->{'quote'}[rand(scalar(@{$json->{'quote'}}))]."\n";
+    close($fh);
+}
+else
+{
+    die "Cannot use an empty file : $file";
+}
+closedir($dh);
